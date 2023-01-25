e2f0a7929c46e2ba22cca243b699b341f99ba5c2
Aeos <aeosdraws@gmail.com>
2023-01-25 13:23:22 -0500

    Case setup for first function

--- Bankingapptest.cpp
+++ Bankingapptest.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include <cctype>
 using namespace std;
 
 int main()
@@ -9,11 +10,26 @@ int main()
     char input{};
     bool end{ false };
     double balance{ 0 };
-    cout << balance;
 
     do {
-        cout << "What would you like to do\n" << "Type 'b' for a balance check\n Type 'w' for a withdraw\n Type 'd' for deposit\n Or press 'q' to quit";
+        //User prompt 
+        cout << "What would you like to do\n" << "Type 'b' for a balance check\nType 'w' for a withdraw\nType 'd' for deposit\nOr press 'q' to quit the program ";
+        cin >> input;
         
+        //Check for upper case characters and convert them to lower case
+        if (isupper(input))
+            tolower(input);
+        
+
+        //Check for input
+        switch (input) {
+            case 'b':
+                cout << "Successful balance check";
+                break;
+            default:
+                cout << "Please insert a valid option listed above";
+                break;
+        }
 
     } while (!end);
 }
