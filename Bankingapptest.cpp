b31c10aba5345480cd7c79b77bde3ea9cfc930cc
Aeos <aeosdraws@gmail.com>
2023-01-25 13:09:44 -0500

    User prompt

--- Bankingapptest.cpp
+++ Bankingapptest.cpp
@@ -6,20 +6,14 @@ using namespace std;
 
 int main()
 {
+    char input{};
+    bool end{ false };
     double balance{ 0 };
     cout << balance;
+
+    do {
+        cout << "What would you like to do\n" << "Type 'b' for a balance check\n Type 'w' for a withdraw\n Type 'd' for deposit\n Or press 'q' to quit";
+        
+
+    } while (!end);
 }
-
-
-
-
-
-
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
